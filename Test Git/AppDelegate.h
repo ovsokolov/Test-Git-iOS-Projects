bcc2e716d5dbc1eb2ad0c1d7396296559aa2b479
Oleg Sokolov <olegsokolov@olegs-air.home>
2014-11-03 23:23:06 -0500

    Initial Commit

---
+++ Test Git/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Test Git
+//
+//  Created by Oleg Sokolov on 11/3/14.
+//  Copyright (c) 2014 Oleg Sokolov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Test Git/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Test Git
+//
+//  Created by Oleg Sokolov on 11/3/14.
+//  Copyright (c) 2014 Oleg Sokolov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
